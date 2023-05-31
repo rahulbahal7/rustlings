db4a4c40a24be035782c644d319b02e6c0cd6e88
Rahul Bahal <rahul.bahal@hpe.com>
2023-05-31 14:14:57 -0700

    save till now, next vecs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,10 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(mut apples : i32) -> i32 {
+    let mut cost = 0;
+    if apples > 40 {
+        cost = apples * 1;
+    } else {
+        cost = apples * 2;
+    }
+    cost
+}
 
 // Don't modify this function!
 #[test]
